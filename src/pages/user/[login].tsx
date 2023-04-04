bd6a6e0f9fe9c800d669af46aa5023d264193c28
cleo-putra <abdigoldi@gmail.com>
2023-04-04 15:03:10 +0700

    update router isfallback4

--- src/pages/user/[login].tsx
+++ src/pages/user/[login].tsx
@@ -11,31 +11,30 @@ import Repo from '../components/Repo'
 //   name: string;
 //   bio?: string;
 // }
-// interface UInfo {
-//   avatar_url: string;
-//   name: string;
-//   location: string;
-//   blog: string;
-//   html_url: string;
-//   [key:string]: string;
-// }
 
 const User = () => {
+  interface UInfo {
+    avatar_url: string;
+    name: string;
+    location: string;
+    blog: string;
+    html_url: string;
+    [key:string]: string;
+  }
   const router = useRouter();
   const login = router?.query?.login;
   
-  const [userInfo, setUserInfo] = useState([]);
+  const [userInfo, setUserInfo] = useState<any>([]);
   const [repos, setRepos] = useState([]);
 
   useEffect(() => {
     const fetchUserInformation = async() => {
       try {
-          const response = await Promise.all([
-            axios.get(`/users/${login}`),
-            axios.get(`/users/${login}/repos`),
-          ]);
-          setUserInfo(response[0].data);
-          setRepos(response[1].data);
+        const responseUser = await axios.get(`/users/${login}`);
+        const responseRepo = await axios.get(`/users/${login}/repos`); 
+
+          setUserInfo(responseUser.data);
+          setRepos(responseRepo.data);
       } catch (error) {
         console.log(error);
       }
@@ -50,33 +49,33 @@ const User = () => {
       <a href="/"><button className='back'>Back</button></a>
       <div className="user-information">
         <div className="image">
-          <img src={userInfo.avatar_url} />
+          <img src={userInfo!.avatar_url} />
         </div>
         <div className="user-content">
-          <h3>{userInfo.name}</h3>
-          <p>{userInfo.bio}</p>
+          <h3>{userInfo!.name}</h3>
+          <p>{userInfo!.bio}</p>
           <div className="more-data">
             <p><img className='logo-icon' src={user.src} alt="user" />{userInfo.followers} Followers | Following {userInfo.following}</p>
-            {userInfo.location && (
+            {userInfo!.location && (
             <p>
               <img className='logo-icon' src={location.src} alt="" />
-              {userInfo.location}
+              {userInfo!.location}
             </p>
             )}
-            {userInfo.blog && (
+            {userInfo!.blog && (
             <p>
-              <img className='logo-icon' src={site.src} alt="" />{userInfo.blog}
+              <img className='logo-icon' src={site.src} alt="" />{userInfo!.blog}
             </p>
             )}
             <p>
               <img className='logo-icon' src={github.src} alt="" />
-              <a style={{cursor: 'pointer', textDecoration: 'underline'}} href={userInfo.html_url} target="_blank">View Github Profile</a>
+              <a style={{cursor: 'pointer', textDecoration: 'underline'}} href={userInfo!.html_url} target="_blank">View Github Profile</a>
             </p>
           </div>
         </div>
       </div>
       <div className="user-repos">
-        {repos ? (repos.map((repo) => {
+        {repos ? (repos.map((repo: any) => {
          return <Repo repo={repo} key={repo.id} />
 })) : (
   <h2>No repository for this user...</h2>
